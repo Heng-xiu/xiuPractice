e0bc5369a55eeef069bb915f9b5102fd851feafe
Heng-xiu <r23456999@gmail.com>
2015-07-24 11:14:19 +0800

    minone mod

--- example/app.jsx
+++ example/app.jsx
@@ -2,9 +2,27 @@ var React = require("react");
 var MDL = require('mdl-react');
 
 var HelloMessage = React.createClass({
+
 	render: function() {
+
+		var links = [
+		    <a href='#'>Link 1</a>,
+		    <a href='#'>Link 2</a>,
+		    <a href='#'>Link 3</a>,
+		];
+
 		return (
-			<div>Hello world</div>
+			<div>
+				<MDL.Layout
+					title='Fixed header and drawer'
+					href='#'
+					isFixedHeader={true}
+					headerLinks={links}
+					drawerLinks={links}
+				>
+					put your content here
+				</MDL.Layout>
+			</div>
 		);
 	}
 });
